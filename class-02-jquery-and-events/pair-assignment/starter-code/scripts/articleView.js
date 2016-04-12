c1ac1753a0e62e4ed12ba64d8d12d30ed1bff29f
dstineback <dstineback@gmail.com>
2016-04-12 14:10:45 -0700

    last update before trading drivers

--- class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
@@ -32,6 +32,8 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+      $('article').hide();
+      $('article').filter();
 
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
