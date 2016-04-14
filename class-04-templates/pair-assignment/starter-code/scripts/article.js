551d63cc366a42435b3e27fef21f744d9efdb28a
Aliza Pilisuk <apilisuk@gmail.com>
2016-04-14 12:57:34 -0700

    use handlebar to render article and made Article handler more simple

--- class-04-templates/pair-assignment/starter-code/scripts/article.js
+++ class-04-templates/pair-assignment/starter-code/scripts/article.js
@@ -1,16 +1,14 @@
 var articles = [];
 
-function Article (opts) {
-  this.author = opts.author;
-  this.authorUrl = opts.authorUrl;
-  this.title = opts.title;
-  this.category = opts.category;
-  this.body = opts.body;
-  this.publishedOn = opts.publishedOn;
-}
+function Article (opts){
+  for (key in opts) this[key] = opts[key];
+};
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
+  var $source = $('#blog-template').html();
+  var template = Handlebars.compile($source);
+
+  // DONE: Use handlebars to render your articles.
   //       - Select your template from the DOM.
   //       - Now "compile" your template with Handlebars.
 
@@ -18,7 +16,7 @@ Article.prototype.toHtml = function() {
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
@@ -30,8 +28,8 @@ rawData.sort(function(a,b) {
 
 rawData.forEach(function(ele) {
   articles.push(new Article(ele));
-})
+});
 
 articles.forEach(function(a){
-  $('#articles').append(a.toHtml())
+  $('#articles').append(a.toHtml());
 });
