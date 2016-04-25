1e637fa83d83846d31ad97157bc2ec0e57ac7094
dstineback <dstineback@gmail.com>
2016-04-25 12:27:23 -0700

    init set up

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -3,6 +3,7 @@
 
   aboutController.index = function() {
     // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+    //just show the about page-from class
   };
 
   module.aboutController = aboutController;
