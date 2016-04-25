0eb6d75ab0ddbaa503bdce346a75b993d81ba683
dstineback <dstineback@gmail.com>
2016-04-25 12:38:49 -0700

    added the scripts links to index, also moved the call function to articleController

--- class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
@@ -3,14 +3,20 @@
 
   // TODO: Create the `articles` table when the controller first loads,
   //  with the code that used to be in index.html:
-
+  Article.createTable();///added this in class
   articlesController.index = function() {
+    //init index page with Arcticle view
     // TODO: Complete this function that kicks off the fetching and rendering
     //  of articles, using the same
     //  code that used to be in index.html:
 
+      Article.fetchAll(articleView.initIndexPage);
+
     // TODO: But wait! There's more: Also be sure to hide all the main section
     //  elements, and reveal the articles section:
+    //when you hit the route, hide this then show this- from class
+    $('main > section').hide();
+    $('#articles').show();
   };
 
   module.articlesController = articlesController;
