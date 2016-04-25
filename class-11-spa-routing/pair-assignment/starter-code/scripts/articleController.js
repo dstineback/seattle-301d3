88317f7adce4163f30ae9302e433e57a1661ac71
dstineback <dstineback@gmail.com>
2016-04-25 12:41:58 -0700

    changed TODO to done. trading off with Drew

--- class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
@@ -1,18 +1,18 @@
 (function(module) {
   var articlesController = {};
 
-  // TODO: Create the `articles` table when the controller first loads,
+  // DONE: Create the `articles` table when the controller first loads,
   //  with the code that used to be in index.html:
   Article.createTable();///added this in class
   articlesController.index = function() {
     //init index page with Arcticle view
-    // TODO: Complete this function that kicks off the fetching and rendering
+    // DONE: Complete this function that kicks off the fetching and rendering
     //  of articles, using the same
     //  code that used to be in index.html:
 
       Article.fetchAll(articleView.initIndexPage);
 
-    // TODO: But wait! There's more: Also be sure to hide all the main section
+    // DONE: But wait! There's more: Also be sure to hide all the main section
     //  elements, and reveal the articles section:
     //when you hit the route, hide this then show this- from class
     $('main > section').hide();
